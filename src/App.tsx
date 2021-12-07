263eba201e7e9963f68d5be512790d1a30d5f2e9
nwaose victor <nwaosevictor1234@gmail.com>
2021-12-07 14:59:15 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export type shopCart = {
+    id: number;
+    category: string;
+    description: string;
+    image: string;
+    price: number;
+    title: string;
+    amount: number;
+};
+
+const App: React.FC = () => {
+    const [data, setData] = useState<any>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const url = "https://fakestoreapi.com/products";
+
+    const fetchData = async (): Promise<any> => {
+        setLoading(true);
+        const response = await fetch(url);
+        try {
+            if (response.ok) {
+                const data = await response.json();
+                setData(data);
+                setLoading(false);
+                console.log(data);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>tweet hello `world`</h1>
+        </div>
+    );
+};
 
 export default App;
